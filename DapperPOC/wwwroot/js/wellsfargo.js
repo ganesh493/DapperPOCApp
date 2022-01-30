0340eea40f0ba28dc478d370aa96a321d7dc4cc1
Ganesh Kalvarala <ganeshkalvarala@outlook.com>
2022-01-30 14:20:45 +0530

    Added wells fargo layout, js, and css files

---
+++ DapperPOC/wwwroot/js/wellsfargo.js
@@ -0,0 +1,8 @@
+﻿
+$(document).ready(function () {
+    var str = $('.dv-text-trade').text().split(" ");
+    $('.dv-text-trade').empty();
+    str.forEach(function (a) {
+        $('.dv-text-trade').append('&nbsp;<span>' + a.slice(0, 1) + '</span>' + a.slice(1))
+    })
+})
