a3cae1d5cc208e0d1ee427b9a28b66289a762236
Ganesh Kalvarala <ganeshkalvarala@outlook.com>
2022-02-04 22:35:16 +0530

    Added a new Wells-fargo image, footer, and ui changes

--- DapperPOC/wwwroot/js/wellsfargo.js
+++ DapperPOC/wwwroot/js/wellsfargo.js
@@ -5,4 +5,15 @@ $(document).ready(function () {
     str.forEach(function (a) {
         $('.dv-text-trade').append('&nbsp;<span>' + a.slice(0, 1) + '</span>' + a.slice(1))
     })
+
+    // get a new date (locale machine date time)
+    var date = new Date();
+    // get the date as a string
+    var n = date.toDateString();
+    // get the time as a string
+    var time = date.toLocaleTimeString();
+
+    // find the html element with the id of time
+    // set the innerHTML of that element to the date a space the time
+    document.getElementById('datetime').innerHTML = n + ' ' + time;
 })
